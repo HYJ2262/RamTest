9ad5ba730c31ec4fdf6e825b6150066a1fccde26
D7703_24 <d7703_24@d7703-24ui-imac.local>
2019-05-23 11:19:26 +0900

    Initial Commit

---
+++ RamTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RamTest
+//
+//  Created by D7703_24 on 2019. 5. 23..
+//  Copyright © 2019년 D7703_24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
